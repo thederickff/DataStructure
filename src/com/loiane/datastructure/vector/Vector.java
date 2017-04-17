eed05d6fc87b60333cf89f21f13a2615eacbbf0d
derickfelix <derickfelix4321@gmail.com>
2017-04-17 15:44:17 -0400

    Lesson2 - Vector

---
+++ src/com/loiane/datastructure/vector/Vector.java
@@ -0,0 +1,13 @@
+package com.loiane.datastructure.vector;
+
+public class Vector {
+	
+	private String[] elements;
+	
+	public Vector(int capacity){
+		this.elements = new String[capacity];
+	}
+	
+	
+	
+}
---
+++ src/com/loiane/datastructure/vector/test/Lesson2.java
@@ -0,0 +1,16 @@
+package com.loiane.datastructure.vector.test;
+
+import com.loiane.datastructure.vector.Vector;
+
+public class Lesson2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Vector vector = new Vector(5);
+		
+		System.out.println(vector);
+	
+	}
+
+}
