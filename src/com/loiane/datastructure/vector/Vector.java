0e77e3f4822d844af4b076c019e5b1ff71b5e3b3
derickfelix <derickfelix4321@gmail.com>
2017-04-17 16:03:12 -0400

    Lesson 3 - adding element on vector

--- src/com/loiane/datastructure/vector/Vector.java
+++ src/com/loiane/datastructure/vector/Vector.java
@@ -1,13 +1,49 @@
 package com.loiane.datastructure.vector;
 
 public class Vector {
-	
+
 	private String[] elements;
+	private int size;
 	
-	public Vector(int capacity){
+	public Vector(int capacity) {
 		this.elements = new String[capacity];
+		this.size = 0;
 	}
+
+	/*public void add(String element) {
+
+		for (int i = 0; i < this.elements.length; i++) {
+			if (this.elements[i] == null) {
+				this.elements[i] = element;
+				break;
+			}
+		}
+
+	}*/
+
+	/*public void add(String element) throws Exception{
+
+		if(this.size < this.elements.length){
+
+			this.elements[this.size] = element;
+			this.size++;
+
+		} else {
+			throw new Exception("Vector is full, it is not possible to add any element!");
+		}
+		
+	}*/
 	
-	
-	
+	public boolean add(String element){
+
+		if(this.size < this.elements.length){
+			this.elements[this.size] = element;
+			this.size++;
+			return true;
+		} else {
+			return false;
+		}
+		
+	}
+
 }
---
+++ src/com/loiane/datastructure/vector/test/Lesson3.java
@@ -0,0 +1,17 @@
+package com.loiane.datastructure.vector.test;
+
+import com.loiane.datastructure.vector.Vector;
+
+public class Lesson3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Vector vector = new Vector(5);
+
+		vector.add("First Element");
+		vector.add("Second Element");
+
+	}
+
+}
