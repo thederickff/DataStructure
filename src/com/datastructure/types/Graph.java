00c2b73f4c611dca5ef4740520f43fb756f6a992
derickfelix <derickfelix4321@gmail.com>
2017-09-20 13:19:07 -0400

    Add Graph Data Structure

---
+++ src/com/datastructure/types/Graph.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.datastructure.types;
+
+/**
+ * @name Graph
+ * @author derickfelix
+ * @date Sep 20, 2017
+ */
+public class Graph {
+
+    private Object[] vertices;
+    private Dictionary adjList;
+    
+    public Graph(int initialLength) {
+        this.vertices = new Object[initialLength];
+        this.adjList = new Dictionary(initialLength);
+    }
+    
+    /**
+     * This adds a new vertex on the graph
+     * @param v the object vertex
+     */
+    public void addVertex(Object v) {
+        
+    }
+}
